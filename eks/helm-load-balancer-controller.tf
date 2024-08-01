3ed6b7f7a13e810eaf9f70552c09103bc743f7eb
Khaja Ehteshamuddin Ahmed <ehteshamkhaja@gmail.com>
2024-08-02 02:45:11 +0300

    Update helm-load-balancer-controller.tf

--- eks/helm-load-balancer-controller.tf
+++ eks/helm-load-balancer-controller.tf
@@ -15,7 +15,7 @@ module "aws_load_balancer_controller_irsa_role" {
 }
 resource "aws_iam_role_policy" "alb-additional-policy" {
   name = "alb-additional-policy"
-  role = module.aws_load_balancer_controller_irsa_role.id
+  role = module.aws_load_balancer_controller_irsa_role.iam_role_arn
 
   policy = <<-EOF
 {
